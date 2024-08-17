009e12c04498c3ce53d6817a9174b7e163576f83
vinhpvdev <vinhpvdev@gmail.com>
2024-08-17 11:14:05 +0700

    init chrome version

--- BrowserLauncher.cs
+++ BrowserLauncher.cs
@@ -8,8 +8,9 @@ namespace PrintifyAutomation
         {
             ProcessStartInfo startInfo = new ProcessStartInfo
             {
-                FileName = @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
-                Arguments = "--remote-debugging-port=9444 --user-data-dir=\"D:\\selenium\\EdgeProfile\"",
+                // Update the path to point to Chrome's executable
+                FileName = @"C:\Program Files\Google\Chrome\Application\chrome.exe",
+                Arguments = "--remote-debugging-port=9445 --user-data-dir=\"D:\\selenium\\ChromeProfile\"",
                 UseShellExecute = false
             };
             Process.Start(startInfo);
